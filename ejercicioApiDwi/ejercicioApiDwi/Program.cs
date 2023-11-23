c622f5d7bf54d12b20e23ad5ea54946ff2f65b84
d-d4nny <danitbp12@gmail.com>
2023-11-23 09:12:20 +0100

    first

---
+++ ejercicioApiDwi/ejercicioApiDwi/Program.cs
@@ -0,0 +1,30 @@
+using ejercicioApiDwi.Contexto;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+     options => options.UseNpgsql(builder.Configuration.GetConnectionString("Connection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
